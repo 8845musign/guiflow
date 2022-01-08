10023ec68552fcc411e00ad7223d17b0ed4fc5d5
8845musign <8845musign@gmail.com>
2022-01-08 21:17:04 +0900

    fix: at least I can start it up

--- js/script.js
+++ js/script.js
@@ -24,11 +24,11 @@ var nodeModule = function() {
 
 };
 var ipcRenderer = require("electron").ipcRenderer;
-var remote = require("remote");
+// var remote = require("remote");
 var fs = require("fs");
 var flumine = require("flumine");
 var $ = require(resolvePath("./js/jquery-2.1.4.min"));
-var uiflow = remote.require("./app/uiflow");
+// var uiflow = remote.require("./app/uiflow");
 var editor = require(resolvePath("./js/editor"));
 var diagram = require(resolvePath("./js/diagram"));
 
@@ -50,98 +50,98 @@ var sendToEditor = function(channel) {
     return editor[channel];
 };
 
-var clipboard = require("clipboard");
-var nativeImage = require("native-image");
+const { clipboard } = require('electron')
+const { nativeImage } = require('electron')
 
-var Menu = remote.require('menu');
-var menu = Menu.buildFromTemplate([{
-    label: "Undo",
-    accelerator: 'CmdOrCtrl+Z',
-    click: sendToEditor("undo"),
-}, {
-    label: "Redo",
-    accelerator: 'CmdOrCtrl+Y',
-    click: sendToEditor("redo"),
-}, {
-    type: 'separator'
-}, {
-    label: "Cut",
-    accelerator: 'CmdOrCtrl+X',
-    click: sendToEditor("cut"),
-}, {
-    label: "Copy",
-    accelerator: 'CmdOrCtrl+C',
-    click: sendToEditor("copy"),
-}, {
-    label: "Paste",
-    accelerator: 'CmdOrCtrl+V',
-    click: sendToEditor("paste"),
-}, {
-    label: "Select All",
-    accelerator: 'CmdOrCtrl+A',
-    click: sendToEditor("selectAll"),
-}, ]);
+// var Menu = remote.require('menu');
+// var menu = Menu.buildFromTemplate([{
+//     label: "Undo",
+//     accelerator: 'CmdOrCtrl+Z',
+//     click: sendToEditor("undo"),
+// }, {
+//     label: "Redo",
+//     accelerator: 'CmdOrCtrl+Y',
+//     click: sendToEditor("redo"),
+// }, {
+//     type: 'separator'
+// }, {
+//     label: "Cut",
+//     accelerator: 'CmdOrCtrl+X',
+//     click: sendToEditor("cut"),
+// }, {
+//     label: "Copy",
+//     accelerator: 'CmdOrCtrl+C',
+//     click: sendToEditor("copy"),
+// }, {
+//     label: "Paste",
+//     accelerator: 'CmdOrCtrl+V',
+//     click: sendToEditor("paste"),
+// }, {
+//     label: "Select All",
+//     accelerator: 'CmdOrCtrl+A',
+//     click: sendToEditor("selectAll"),
+// }, ]);
 
-window.addEventListener('contextmenu', function(e) {
-    e.preventDefault();
-    menu.popup(remote.getCurrentWindow());
-}, false);
+// window.addEventListener('contextmenu', function(e) {
+//     e.preventDefault();
+//     menu.popup(remote.getCurrentWindow());
+// }, false);
 
 var dialogs = require("dialogs")({});
 
 
-$(function() {
+// $(function() {
 
 
-    $(window).on("load resize", function() {
-        $(".main").height($(window).height());
-    });
-    $("#download").click(function(e) {
-        editor.value.and(function(code) {
-            return uiflow.update("<anon>", code, "svg");
-        }).and(function(svg) {
+//     $(window).on("load resize", function() {
+//         $(".main").height($(window).height());
+//     });
+//     $("#download").click(function(e) {
+//         editor.value.and(function(code) {
+//             return uiflow.update("<anon>", code, "svg");
+//         }).and(function(svg) {
 
-            var image = new Image;
-            var strSvg = String(svg);
-            var match = strSvg.match(/svg width="(\d+)pt" height="(\d+)pt"/);
-            var width = match[1];
-            var height = match[2];
+//             var image = new Image;
+//             var strSvg = String(svg);
+//             var match = strSvg.match(/svg width="(\d+)pt" height="(\d+)pt"/);
+//             var width = match[1];
+//             var height = match[2];
 
-            image.src = "data:image/svg+xml," + encodeURIComponent(svg);
-            var cElement = document.createElement("canvas");
-            cElement.width = width * 2;
-            cElement.height = height * 2;
-            var cContext = cElement.getContext("2d");
-            cContext.fillStyle = "#fff";
-            cContext.fillRect(-10, -10, width * 3, height * 3);
-            cContext.drawImage(image, 0, 0, width * 2, height * 2);
-            var png = cElement.toDataURL("image/png");
+//             image.src = "data:image/svg+xml," + encodeURIComponent(svg);
+//             var cElement = document.createElement("canvas");
+//             cElement.width = width * 2;
+//             cElement.height = height * 2;
+//             var cContext = cElement.getContext("2d");
+//             cContext.fillStyle = "#fff";
+//             cContext.fillRect(-10, -10, width * 3, height * 3);
+//             cContext.drawImage(image, 0, 0, width * 2, height * 2);
+//             var png = cElement.toDataURL("image/png");
 
-            var image = nativeImage.createFromDataURL(png);
-            clipboard.writeImage(image);
+//             var image = nativeImage.createFromDataURL(png);
+//             clipboard.writeImage(image);
 
-            alert("Copied Image to Clipboard");
-        })();
-    });
+//             alert("Copied Image to Clipboard");
+//         })();
+//     });
 
-    editor.on("change", function(code) {
-        uiflow.compile(code).then(function(data) {
-                editor.clearError();
-                return data;
-            })
-            .then(diagram.refresh)
-            .catch(editor.setError);
-    });
-    editor.on("same", function(fileName) {
-        document.title = "guiflow -- " + (fileName || "Untitled") + " = ";
-    });
-    editor.on("diff", function(fileName) {
-        document.title = "guiflow -- " + (fileName || "Untitled") + " + ";
-    });
-    diagram.on("page-click", function(lines) {
-        editor.navigateTo(lines);
-    });
-    diagram.on("end-click", function(text) {
-        editor.insert(text);
-    });
-});
+//     editor.on("change", function(code) {
+//         uiflow.compile(code).then(function(data) {
+//                 editor.clearError();
+//                 return data;
+//             })
+//             .then(diagram.refresh)
+//             .catch(editor.setError);
+//     });
+//     editor.on("same", function(fileName) {
+//         document.title = "guiflow -- " + (fileName || "Untitled") + " = ";
+//     });
+//     editor.on("diff", function(fileName) {
+//         document.title = "guiflow -- " + (fileName || "Untitled") + " + ";
+//     });
+//     diagram.on("page-click", function(lines) {
+//         editor.navigateTo(lines);
+//     });
+//     diagram.on("end-click", function(text) {
+//         editor.insert(text);
+//     });
+// });
